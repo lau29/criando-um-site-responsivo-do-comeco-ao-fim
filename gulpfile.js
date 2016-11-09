6ff87187b2ab230aa5c5efe12e48e12d51d9eb7a
lau <lauspbv@gmail.com>
2016-11-09 11:27:44 -0200

    '09112016'

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+
+//task para o sass
+gulp.task('sass',function(){
+    return sass('sass/*.sass');
+});
+
+//task default gulp
+gulp.task('default',function(){
+    console.log('olá coders!');
+});
