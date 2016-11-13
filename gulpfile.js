562ecf44354d43d55229f8431e48f9826191def8
lau <lauspbv@gmail.com>
2016-11-13 13:34:46 -0200

    commit dia 13

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 var gulp = require('gulp');
-var sass = require('gulp-ruby-sass');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
 
 //task para o sass
 gulp.task('sass',function(){
-    return sass('sass/*.sass').pipe(gulp.dest('css'))
+    return gulp.src('sass/**/*.sass')
+        .pipe(sass({outputStyle:'compressed'}).on('error',sass.logError))
+        .pipe(gulp.dest('css'))
+});
+
+//task para o watch
+gulp.task('watch',function(){
+   gulp.watch('sass/**/*.sass',['sass']);
 });
 
 //task default gulp
-gulp.task('default',function(){
-    console.log('olá coders!');
-});
+gulp.task('default',['sass','watch']);
